d5bb5e75aadae665146f41d5347174cf0eb2164f
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-05 03:18:45 -0700

    rename

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <div id="page-body">Welcom to my blog!</div>
+    </div>
+  );
+}
+
+export default App;
