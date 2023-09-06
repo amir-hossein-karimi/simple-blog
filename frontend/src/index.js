43ecd925dccac9ec50289dfdae7b24fd6f482916
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-06 04:32:45 -0700

    add firebase config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDL2s3Fj-n5d4XNTbwi8NXJWkDF373Fras",
+  authDomain: "simple-blog-28360.firebaseapp.com",
+  projectId: "simple-blog-28360",
+  storageBucket: "simple-blog-28360.appspot.com",
+  messagingSenderId: "608099153113",
+  appId: "1:608099153113:web:fca1e20d0f328d8f83fac8",
+};
+
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
